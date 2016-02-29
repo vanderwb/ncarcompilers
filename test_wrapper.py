eb39f6c2b903d6b7903b169c40516cab1b5c48a7
Davide Del Vento <davide.del.vento@gmail>
2016-02-29 16:57:45 -0700

    ability to call the external program

--- test_wrapper.py
+++ test_wrapper.py
@@ -5,10 +5,14 @@ def check_output(cmd):
     return subprocess.check_output(subprocess.list2cmdline(cmd), shell=True).strip()
 
 def test_name():
-    for name in ["wrapper.py", "link-to-wrapper"]:
+    for name in ["wrapper.py", "link-to-wrapper", "gcc"]:
         print name
         assert check_output(["./" + name, "---ncardebug-print-compiler-name"]) == name
 
 def test_quotes():
     quoted_argument = "This is a filename with spaces.exe"
     assert check_output(["./wrapper.py", "---ncardebug-print-arg", quoted_argument]) == quoted_argument
+
+def test_callexternal():
+    results = check_output(["./gcc", "--version"])
+    assert "Free Software Foundation" in results
--- wrapper.py
+++ wrapper.py
@@ -4,9 +4,16 @@ import os, sys, subprocess
 def compiler():
     return os.path.basename(__file__)
 
+def invoke():
+    my_path = subprocess.check_output("which " + compiler(), shell=True).strip()
+    subprocess.call([my_path] + sys.argv[1:])
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "---ncardebug-print-compiler-name":
             print compiler()
+            sys.exit(0)
         if sys.argv[1] == "---ncardebug-print-arg":
             print sys.argv[2]
+            sys.exit(0)
+    invoke()
