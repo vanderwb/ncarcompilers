7f5dbbd7d8018702be7e8577c9cb39405770a5be
Davide Del Vento <davide.del.vento@gmail>
2016-02-29 16:31:59 -0700

    avoid referencing array out of bound

--- wrapper.py
+++ wrapper.py
@@ -5,7 +5,8 @@ def compiler():
     return sys.argv[0] # can be os.path.basename(__file__)
 
 if __name__ == "__main__":
-    if sys.argv[1] == "---ncardebug-print-compiler-name":
-        print compiler()
-    if sys.argv[1] == "---ncardebug-print-arg":
-        print sys.argv[2]
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "---ncardebug-print-compiler-name":
+            print compiler()
+        if sys.argv[1] == "---ncardebug-print-arg":
+            print sys.argv[2]
