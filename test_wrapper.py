0d38494ebf9c3567b42ddba2695e7a29026d48cd
Davide Del Vento <davide.del.vento@gmail>
2016-02-29 14:30:40 -0700

    stripping useless newlines when testing

--- test_wrapper.py
+++ test_wrapper.py
@@ -4,4 +4,4 @@ import subprocess
 def test_name():
     for name in ["./wrapper.py", "./link-to-wrapper"]:
         print name
-        assert subprocess.check_output([name, "---debug-ncar-wrapper"]) == name
+        assert subprocess.check_output([name, "---debug-ncar-wrapper"]).strip() == name
