7cb3d55676f7a618f8c81e8ab1c6d15d32dbc89f
Davide Del Vento <davide.del.vento@gmail>
2016-02-29 15:15:05 -0700

    refactoring the compiler name

--- test_wrapper.py
+++ test_wrapper.py
@@ -4,4 +4,4 @@ import subprocess
 def test_name():
     for name in ["./wrapper.py", "./link-to-wrapper"]:
         print name
-        assert subprocess.check_output([name, "---debug-ncar-wrapper"]).strip() == name
+        assert subprocess.check_output([name, "---ncardebug-print-compiler-name"]).strip() == name
--- wrapper.py
+++ wrapper.py
@@ -1,7 +1,9 @@
 #!/bin/env python
 import os, sys
 
+def compiler():
+    return sys.argv[0] # can be os.path.basename(__file__)
+
 if __name__ == "__main__":
-    if sys.argv[1] == "---debug-ncar-wrapper":
-        #print "OS way", os.path.basename(__file__)
-        print sys.argv[0]
+    if sys.argv[1] == "---ncardebug-print-compiler-name":
+        print compiler()
