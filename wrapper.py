9f2a144f37a09f90065d6c6d07ffdcc22525ac78
Davide Del Vento <davide.del.vento@gmail>
2016-03-01 15:20:18 -0700

    refactoring more clear names

--- wrapper.py
+++ wrapper.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import os, sys, subprocess
 
-def compiler():
+def compiler_name():
     return os.path.basename(__file__)
 
 def invoke():
-    my_path = subprocess.check_output("which " + compiler(), shell=True).strip()
-    subprocess.call([my_path] + sys.argv[1:]) # this should use shell
+    compiler_name_with_path = subprocess.check_output("which " + compiler_name(), shell=True).strip()
+    subprocess.call([compiler_name_with_path] + sys.argv[1:]) # this should use shell
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "---ncardebug-print-compiler-name":
-            print compiler()
+            print compiler_name()
             sys.exit(0)
         if sys.argv[1] == "---ncardebug-print-arg":
             print sys.argv[2]
