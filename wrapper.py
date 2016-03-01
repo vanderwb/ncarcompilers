02eba46c4ff6d8d99445e3f915e891cc49b9f03c
Davide Del Vento <davide.del.vento@gmail>
2016-03-01 15:58:17 -0700

    implementation

--- wrapper.py
+++ wrapper.py
@@ -17,6 +17,9 @@ def include_str():
 def ldflags_str():
     return create_str(parse_env = 'NCAR_LDFLAGS_', joinwith='-L')
 
+def rpath_str():
+    return create_str(parse_env = 'NCAR_LDFLAGS_', joinwith='-Wl,-rpath,')
+
 def invoke():
     compiler_name_with_path = subprocess.check_output("which " + compiler_name(), shell=True).strip()
     subprocess.call([compiler_name_with_path] + sys.argv[1:]) # this should use shell
