5a1b5f3cea8acd2bcda85dffd5fc8183172df837
Davide Del Vento <davide.del.vento@gmail>
2016-03-01 15:21:15 -0700

    include_str implementation, passing the first test

--- wrapper.py
+++ wrapper.py
@@ -4,6 +4,13 @@ import os, sys, subprocess
 def compiler_name():
     return os.path.basename(__file__)
 
+def include_str():
+    inc = ""
+    for key, value in os.environ.iteritems():
+        if key.startswith('NCAR_INC_'):
+            inc += "-I" + value + " "
+    return inc
+
 def invoke():
     compiler_name_with_path = subprocess.check_output("which " + compiler_name(), shell=True).strip()
     subprocess.call([compiler_name_with_path] + sys.argv[1:]) # this should use shell
