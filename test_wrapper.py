d3a7cfebe9bf91ceb89f5b3a24ee808c6e0fa657
Davide Del Vento <davide.del.vento@gmail>
2016-03-01 15:16:54 -0700

    TDD of the include string - single thing library

--- test_wrapper.py
+++ test_wrapper.py
@@ -21,6 +21,16 @@ def test_callexternal():
     results = check_output(["./gcc", "--version"])
     assert "Free Software Foundation" in results
 
+def test_single_include():
+    results = wrapper.include_str() # before the env var is set
+    env = os.environ;
+    env['NCAR_INC_FOO'] = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/include'
+    assert not "-I" + env['NCAR_INC_FOO'] in results
+
+    results = wrapper.include_str() # after the env var is set
+    assert "-I" + env['NCAR_INC_FOO'] in results
+    del env['NCAR_INC_FOO']
+
 if __name__ == "__main__":
     import test_helper
     test_helper.help()
