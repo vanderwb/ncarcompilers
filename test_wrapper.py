f4ca9a4c7d162a1be6cc7f301bc5f27c24da8202
Davide Del Vento <davide.del.vento@gmail>
2017-03-03 13:49:06 -0700

    Isolate all tests from the real environment by using a stub

--- test_wrapper.py
+++ test_wrapper.py
@@ -48,67 +48,62 @@ def test_callexternal():
     results = check_output(["./gcc", "--version"])
     assert "Free Software Foundation" in results
 
-def test_single_include():
+def test_single_include(stub_env):
     name = 'NCAR_INC_FOO'
     value = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/include'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.include_str() # without the env var
     assert not "-I" + value in results
 
-    env[name] = value
+    stub_env[name] = value
     results = wrapper.include_str() # with the env var
     assert "-I" + value in results
-    del env[name]
 
-def test_multiple_includes():
+def test_multiple_includes(stub_env):
     name_foo = 'NCAR_INC_FOO'
     name_bar = 'NCAR_INC_BAR'
     value_foo = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/include'
     value_bar = '/glade/apps/opt/bar/7.8.9/intel/10.11.12/include'
 
-    env = os.environ
-    env[name_foo] = value_foo
-    env[name_bar] = value_bar 
+    wrapper.os.environ = stub_env
+    stub_env[name_foo] = value_foo
+    stub_env[name_bar] = value_bar
 
     results = wrapper.include_str() # with the env var
-    assert "-I" + env[name_foo] in results
-    assert "-I" + env[name_bar] in results
+    assert "-I" + stub_env[name_foo] in results
+    assert "-I" + stub_env[name_bar] in results
     # maybe should check also the space in between, but that's too much logic for a unit test
-    del env[name_foo]
-    del env[name_bar]
 
-def test_multiple_includes_together():
+def test_multiple_includes_together(stub_env):
     name = 'NCAR_INC_FOO'
     value_foo = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/include'
     value_bar = '/glade/apps/opt/bar/7.8.9/intel/10.11.12/include'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.include_str() # without the env var
     assert not "-I" + value_foo in results
     assert not "-I" + value_bar in results
 
-    env[name] = value_foo + ":" + value_bar
+    stub_env[name] = value_foo + ":" + value_bar
     results = wrapper.include_str() # with the env var
     assert "-I" + value_foo in results
     assert "-I" + value_bar in results
-    del env[name]
 
-def test_single_ldflag():
+def test_single_ldflag(stub_env):
     name = 'NCAR_LDFLAGS_FOO'
     value = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/lib'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.ldflags_str() # without the env var
     assert not "-L" + value in results
 
-    env[name] = value
+    stub_env[name] = value
     results = wrapper.ldflags_str() # with the env var
     assert "-L" + value in results
-    del env[name]
 
 def test_multiple_ranked_ldflags(stub_env):
     helper_test_multiple_ranked_ldflags('0', stub_env)
@@ -130,109 +125,99 @@ def helper_test_multiple_ranked_ldflags(value, env):
     results = wrapper.ldflags_str() # with the env var
     expected = "-L" + env[name_foo] + " " + "-L" + env[name_bar]
     assert " ".join(results.split()) == expected.strip()
-    del env[name_foo]
-    del env[name_bar]
-    del env['NCAR_RANK_FOO']
-    del env['NCAR_RANK_BAR']
 
-def test_multiple_ldflags_together():
+def test_multiple_ldflags_together(stub_env):
     name = 'NCAR_LDFLAGS_FOO'
     value_foo = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/lib'
     value_bar = '/glade/apps/opt/bar/7.8.9/intel/10.11.12/lib'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.ldflags_str() # without the env var
     assert not "-L" + value_foo in results
     assert not "-L" + value_bar in results
 
-    env[name] = value_foo + ":" + value_bar
+    stub_env[name] = value_foo + ":" + value_bar
     results = wrapper.ldflags_str() # with the env var
     assert "-L" + value_foo in results
     assert "-L" + value_bar in results
-    del env[name]
 
-def test_single_rpath():
+def test_single_rpath(stub_env):
     RPATH_FLAG = "-Wl,-rpath," # this depends on the compiler and it is so for gcc and intel, will add pgi later
     name = 'NCAR_LDFLAGS_FOO'
     value = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/lib'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.rpath_str() # without the env var
     assert not RPATH_FLAG + value in results
 
-    env[name] = value
+    stub_env[name] = value
     results = wrapper.rpath_str() # with the env var set
     assert RPATH_FLAG + value in results
-    del env[name]
 
 # no need to test multiple rpath, since the code exercising this is
 # exactly the same for multiple includes, however a quick test for
 # the multiple rpath together is appropriate to make sure it won't
 # be broken in the future
 
-def test_multiple_rpath_together():
+def test_multiple_rpath_together(stub_env):
     RPATH_FLAG = "-Wl,-rpath," # this depends on the compiler and it is so for gcc and intel, will add pgi later
     name = 'NCAR_LDFLAGS_FOO'
     value_foo = '/glade/apps/opt/foo/1.2.3/gcc/3.4.5/lib'
     value_bar = '/glade/apps/opt/bar/7.8.9/intel/10.11.12/lib'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.ldflags_str() # without the env var
     assert not RPATH_FLAG + value_foo in results
     assert not RPATH_FLAG + value_bar in results
 
-    env[name] = value_foo + ":" + value_bar
+    stub_env[name] = value_foo + ":" + value_bar
     results = wrapper.rpath_str() # with the env var set
     assert RPATH_FLAG + value_foo in results
     assert RPATH_FLAG + value_bar in results
-    del env[name]
 
-def test_single_linklib():
+def test_single_linklib(stub_env):
     name = 'NCAR_LIBS_FOO'
     value = '-lfooc -lfoof -lfoo'
 
-    env = os.environ
-    env.pop(name, None)
+    wrapper.os.environ = stub_env
+    stub_env.pop(name, None)
     results = wrapper.linklib_str() # without the env var
     assert not value in results
 
-    env[name] = value
+    stub_env[name] = value
     results = wrapper.linklib_str() # with the env var set
     assert value in results
-    del env[name]
 
-def test_overriding_linklibs():
+def test_overriding_linklibs(stub_env):
     name_foo = 'NCAR_LIBS_FOO'
     name_bar = 'NCAR_LIBS_BAR'
     value_foo = '-lfooc -lfoof -lfoo'
     value_bar = '-lbar'
 
-    env = os.environ
-    env[name_foo] = value_foo
-    env[name_bar] = value_bar
+    wrapper.os.environ = stub_env
+    stub_env[name_foo] = value_foo
+    stub_env[name_bar] = value_bar
     results = wrapper.linklib_str()
     assert value_foo in results
     assert value_bar in results
 
-    env['NCAR_EXCLUDE_LIBS'] = "1"
+    stub_env['NCAR_EXCLUDE_LIBS'] = "1"
     results = wrapper.linklib_str()
     assert not value_foo in results
     assert not value_bar in results
 
-    env['NCAR_EXCLUDE_LIBS'] = "False"
+    stub_env['NCAR_EXCLUDE_LIBS'] = "False"
     results = wrapper.linklib_str()
     assert not value_foo in results
     assert not value_bar in results
 
-    del env['NCAR_EXCLUDE_LIBS']
+    del stub_env['NCAR_EXCLUDE_LIBS']
     results = wrapper.linklib_str()
     assert value_foo in results
     assert value_bar in results
-    del env[name_foo]
-    del env[name_bar]
 
 def test_clean_arguments():
     cli_args = ["-lfoo", "-lnetcdff", "-lbar"]      # command line arguments come as a list
